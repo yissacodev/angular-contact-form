7596e71482c6217b7871ed7fd54315dde32b8c05
acosta032 <yesid9523@gmail.com>
2023-11-24 11:24:28 -0500

    First commit

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+  name = new FormControl('', Validators.required);
+  last = new FormControl('', Validators.required);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  phone = new FormControl('');
+  area = new FormControl('');
+  msg = new FormControl('');
+}
