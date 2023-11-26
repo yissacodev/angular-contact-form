636986a4e6917a9fe5c0d0e5340cc7fd407b060b
acosta032 <yesid9523@gmail.com>
2023-11-26 18:22:52 -0500

    API TEST added to form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,18 +7,57 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
-  name = new FormControl('', Validators.required);
-  last = new FormControl('', Validators.required);
-  email = new FormControl('', [Validators.required, Validators.email]);
-  phone = new FormControl('');
-  area = new FormControl('');
-  msg = new FormControl('');
 
-  submit($event: any){
+  formUser = new FormGroup(
+    {
+      'toemail': new FormControl('', Validators.required),
+      'name': new FormControl('', Validators.required),
+      'last': new FormControl('', Validators.required),
+      'email': new FormControl('', [Validators.required, Validators.email]),
+      'phone': new FormControl('', [Validators.required]),
+      'area': new FormControl(''),
+      'msg': new FormControl(''),
+    }
+  );
+
+  get name() {
+    return this.formUser.get('name') as FormControl;
+  }
+  get email() {
+    return this.formUser.get('email') as FormControl;
+  }
+  get toEmail() {
+    return this.formUser.get('toemail') as FormControl;
+  }
+  submit($event: any) {
+    console.log($event);
+  }
+
+  process( $event: any ){
     $event.preventDefault();
-    $event.target.
+    console.log($event.target)
 
-  
-    console.log();
+    let toEmail = this.formUser.get('toemail');
+
+      fetch("https://formsubmit.co/ajax/" + toEmail?.value, {
+          method: "POST",
+          body: new FormData( $event.target )
+      })
+      .then( res => (res.ok ? res.json() : Promise.reject(res)) )
+      .then( json => {
+          console.log(json);
+         
+      })
+      .catch( err => {
+          console.log(err);
+          let message = err.statusText || "Ocurrio un error al enviar, intenta nuevamente";
+          
+      })
+      .finally(() => {
+          setTimeout(() => {
+              
+          }, 3000);
+      })
   }
 }
+
